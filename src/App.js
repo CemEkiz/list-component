3d71f8a6982bcbb980fbe0793f0046e90012e42e
Ekiz Cem <ekizcem.pro@gmail.com>
2022-07-21 19:07:13 +0200

    List Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	//// STATE (état, donnée)
+	const [fruits, setFruits] = useState([
+		{ id: 1, name: 'Abricot' },
+		{ id: 2, name: 'Banane' },
+		{ id: 3, name: 'Cerise' },
+	]);
+
+	const [newFruit, setNewFruit] = useState('');
+
+	//// BEHAVIOR (comportement)
+	const handleDelete = (id) => {
+		// 1. Copier le state
+		const fruitsCopy = [...fruits];
+
+		// 2. Manipuler le state
+		const fruitsCopyUpdated = fruitsCopy.filter((fruit) => fruit.id !== id);
+
+		// 3. Modifier le state avec le setter
+		setFruits(fruitsCopyUpdated);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		// 1. Copier le state
+		const fruitsCopy = [...fruits];
+
+		// 2. Manipuler le state
+		const id = new Date().getTime();
+		const name = newFruit;
+		// const fruitToAdd = { id: id, name: name };
+		const fruitToAdd = { id, name };
+		fruitsCopy.push(fruitToAdd);
+
+		// 3. Modifier le state avec son setter
+		setFruits(fruitsCopy);
+		setNewFruit('');
+	};
+
+	const handleChange = (e) => {
+		setNewFruit(e.target.value);
+	};
+
+	//// RENDER (affichage, rendu)
+	return (
+		<div>
+			<h1>Liste de fruits</h1>
+
+			<ul>
+				{fruits.map((fruit) => (
+					<li key={fruit.id}>
+						{fruit.name}{' '}
+						<button onClick={() => handleDelete(fruit.id)}>X</button>
+					</li>
+				))}
+			</ul>
+
+			<form action='submit' onSubmit={handleSubmit}>
+				<input
+					value={newFruit}
+					type='text'
+					placeholder='Ajouter un fruit'
+					onChange={handleChange}
+				/>
+				<button>Ajouter</button>
+			</form>
+		</div>
+	);
 }
 
 export default App;
