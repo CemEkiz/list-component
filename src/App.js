a3e278792fd6cc7c0b009470e6753eecd111950c
Ekiz Cem <ekizcem.pro@gmail.com>
2022-07-22 13:16:32 +0200

    commenting handleDelete function

--- src/App.js
+++ src/App.js
@@ -11,16 +11,16 @@ function App() {
 	]);
 
 	//// BEHAVIOR (comportement)
-	const handleDelete = (id) => {
-		// 1. Copier le state
-		const fruitsCopy = [...fruits];
+	// const handleDelete = (id) => {
+	// 	// 1. Copier le state
+	// 	const fruitsCopy = [...fruits];
 
-		// 2. Manipuler le state
-		const fruitsCopyUpdated = fruitsCopy.filter((fruit) => fruit.id !== id);
+	// 	// 2. Manipuler le state
+	// 	const fruitsCopyUpdated = fruitsCopy.filter((fruit) => fruit.id !== id);
 
-		// 3. Modifier le state avec le setter
-		setFruits(fruitsCopyUpdated);
-	};
+	// 	// 3. Modifier le state avec le setter
+	// 	setFruits(fruitsCopyUpdated);
+	// };
 
 	const handleAdd = (fruitToAdd) => {
 		// 1. Copier le state
